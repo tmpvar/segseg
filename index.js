d08d9ab5c587d1c8a37f79ee86ddb73c88dd9caa
Elijah Insua <tmpvar@gmail.com>
2012-12-16 15:37:01 -0700

    Use a more robust segment-segment isect algo
    Currently using a modified port of some graphics gems 2 code which
    also tracks colinearity.

--- index.js
+++ index.js
@@ -1,22 +1,77 @@
-module.exports = function(x1, y1, x2, y2, x3, y3, x4, y4) {
- var tp, tq, parallel;
+/*  Ported from Mukesh Prasad's public domain code:
+ *    http://tog.acm.org/resources/GraphicsGems/gemsii/xlines.c
+ *
+ *   This function computes whether two line segments,
+ *   respectively joining the input points (x1,y1) -- (x2,y2)
+ *   and the input points (x3,y3) -- (x4,y4) intersect.
+ *   If the lines intersect, the return value is an array
+ *   containing coordinates of the point of intersection.
+ *
+ *   Params
+ *        x1, y1,  x2, y2   Coordinates of endpoints of one segment.
+ *        x3, y3,  x4, y4   Coordinates of endpoints of other segment.
+ *
+ *
+ *   The value returned by the function is one of:
+ *
+ *        undefined - no intersection
+ *        array     - intersection
+ *        true      - colinear
+ *
+ * Error conditions:
+ *
+ *     Depending upon the possible ranges, and particularly on 16-bit
+ *     computers, care should be taken to protect from overflow.
+ *
+ */
 
-  // Test if the lines are parallel to each other
-  parallel = (x2 - x1) * (y4 - y3) - (y2 - y1) * (x4 - x3)
-  if (parallel === 0) {
-    return null;
+module.exports = function(x1, y1, x2, y2, x3, y3, x4, y4) {
+  var a1, a2, b1, b2, c1, c2; // Coefficients of line eqns.
+  var r1, r2, r3, r4;         // 'Sign' values
+  var denom, offset, num;     // Intermediate values
+
+  // Compute a1, b1, c1, where line joining points 1 and 2
+  // is "a1 x  +  b1 y  +  c1  =  0".
+  a1 = y2 - y1;
+  b1 = x1 - x2;
+  c1 = x2 * y1 - x1 * y2;
+
+  // Compute r3 and r4.
+  r3 = a1 * x3 + b1 * y3 + c1;
+  r4 = a1 * x4 + b1 * y4 + c1;
+
+  // Check signs of r3 and r4.  If both point 3 and point 4 lie on
+  // same side of line 1, the line segments do not intersect.
+  if ( r3 != 0 && r4 != 0 && ((r3 >= 0 && r4 >= 0) || (r3 < 0 && r4 < 0))) {
+    return; // no intersection
   }
 
-  // Colinearity test
-  tp = ((x3 - x1) * (y4 - y3) - (y3 - y1) * (x4 - x3))/parallel;
-  tq = ((y2 - y1) * (x3 - x1) - (x2 - x1) * (y3 - y1))/parallel;
+  // Compute a2, b2, c2
+  a2 = y4 - y3;
+  b2 = x3 - x4;
+  c2 = x4 * y3 - x3 * y4;
 
-  if (tp < 0 || tp > 1 || tq < 0 || tq > 1) {
+  // Compute r1 and r2
+  r1 = a2 * x1 + b2 * y1 + c2;
+  r2 = a2 * x2 + b2 * y2 + c2;
+
+  // Check signs of r1 and r2.  If both point 1 and point 2 lie
+  // on same side of second line segment, the line segments do
+  // not intersect.
+  if (r1 != 0 && r2 != 0 && ((r1 >= 0 && r2 >= 0) || (r1 < 0 && r2 < 0))) {
+    return; // no intersections
+  }
+
+  // Line segments intersect: compute intersection point.
+  denom = a1 * b2 - a2 * b1;
+  console.log('denom', denom);
+  if ( denom == 0 ) {
     return true;
   }
+  offset = denom < 0 ? - denom / 2 : denom / 2;
 
   return [
-    x1 + tp * (x2 - x1),
-    y1 + tp * (y2 - y1)
+    b1 * c2 - b2 * c1,
+    a2 * c1 - a1 * c2
   ];
-};
+}
--- test/index.js
+++ test/index.js
@@ -1,4 +1,37 @@
 var t = require('assert');
 var segseg = require('../');
 
+
+/*
+
+  Basic intersection
+
+                (0, 5)
+                   o
+                   |
+  (-10, 0) o--------+-------o  (10, 0)
+                   |
+                   o
+                (0, -5)
+
+*/
 t.deepEqual(segseg(-10, 0, 10, 0, 0, 5, 0, -5), [0,0])
+
+/*
+
+  Colinear
+             (-2, 0)    (2, 0)
+  (-10, 0) o----o--------o-----o  (10, 0)
+
+*/
+t.deepEqual(segseg(-10, 0, 10, 0, -2, 0, 2, 0), true)
+
+/*
+  No intersection (parallel)
+
+  (-10, 5) o-------------o (10, 5)
+
+  (-10, 0) o-------------o (10, 0)
+
+*/
+t.deepEqual(segseg(-10, 0, 10, 0, -10, 5, 10, 5), undefined)
